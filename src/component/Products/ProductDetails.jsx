468d6f5b8c8c6f7f23560a711b49e33efeda0d6e
Ankit Mishra <aktmishra72@gmail.com>
2025-04-14 18:02:04 +0530

    FeaturedSection, FeaturesSection Added

--- src/component/Products/ProductDetails.jsx
+++ src/component/Products/ProductDetails.jsx
@@ -150,8 +150,8 @@ const ProductDetails = () => {
   }, []);
 
   return (
-    <div className="p-6">
-      <div className="max-w-6xl mx-auto bg-white p-2 rounded-lg">
+    <div className=" px-4">
+      <div className="max-w-6xl mx-auto bg-white rounded-lg">
         <div className="flex flex-col md:flex-row">
           {/* Left Thumbnails */}
           <div className="hidden md:flex flex-col space-y-4 mr-6 ">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,98 +4,100 @@ import GenderCollection from "../component/Products/GenderCollection";
 import NewArrival from "../component/Products/NewArrival";
 import ProductDetails from "../component/Products/ProductDetails";
 import ProductGrid from "../component/Products/ProductGrid";
+import FeaturedCollection from "../component/Products/FeaturedCollection";
+import FeaturesSection from "../component/Products/FeaturesSection";
 
 const Home = () => {
-const placeholderProducts =[
-  {
-    _id: "1",
-    name: "Shirt",
-    price: 120,
-    images: [
-      {
-        url: "https://picsum.photos/500/500?random=1",
-        altText: "Stylish Jacket",
-      },
-    ],
-  },
-  {
-    _id: "2",
-    name: "Jeans",
-    price: 120,
-    images: [
-      {
-        url: "https://picsum.photos/500/500?random=2",
-        altText: "Jeans",
-      },
-    ],
-  },
-  {
-    _id: "3",
-    name: "T-shirt",
-    price: 120,
-    images: [
-      {
-        url: "https://picsum.photos/500/500?random=3",
-        altText: "T-shirt",
-      },
-    ],
-  },
-  {
-    _id: "4",
-    name: "Stylis Jacket",
-    price: 120,
-    images: [
-      {
-        url: "https://picsum.photos/500/500?random=4",
-        altText: "Stylish Jacket",
-      },
-    ],
-  },
-  {
-    _id: "5",
-    name: "Stylis Jacket",
-    price: 120,
-    images: [
-      {
-        url: "https://picsum.photos/500/500?random=5",
-        altText: "Stylish Jacket",
-      },
-    ],
-  },
-  {
-    _id: "6",
-    name: "Stylis Jacket",
-    price: 120,
-    images: [
-      {
-        url: "https://picsum.photos/500/500?random=6",
-        altText: "Stylish Jacket",
-      },
-    ],
-  },
-  {
-    _id: "7",
-    name: "Stylis Jacket",
-    price: 120,
-    images: [
-      {
-        url: "https://picsum.photos/500/500?random=7",
-        altText: "Stylish Jacket",
-      },
-    ],
-  },
-  {
-    _id: "8",
-    name: "Stylis Jacket",
-    price: 120,
-    images: [
-      {
-        url: "https://picsum.photos/500/500?random=8",
-        altText: "Stylish Jacket",
-      },
-    ],
-  },
-]; 
+  const placeholderProducts = [
+    {
+      _id: "1",
+      name: "Shirt",
+      price: 120,
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=1",
+          altText: "Stylish Jacket",
+        },
+      ],
+    },
+    {
+      _id: "2",
+      name: "Jeans",
+      price: 120,
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=2",
+          altText: "Jeans",
+        },
+      ],
+    },
+    {
+      _id: "3",
+      name: "T-shirt",
+      price: 120,
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=3",
+          altText: "T-shirt",
+        },
+      ],
+    },
+    {
+      _id: "4",
+      name: "Stylis Jacket",
+      price: 120,
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=4",
+          altText: "Stylish Jacket",
+        },
+      ],
+    },
+    {
+      _id: "5",
+      name: "Stylis Jacket",
+      price: 120,
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=5",
+          altText: "Stylish Jacket",
+        },
+      ],
+    },
+    {
+      _id: "6",
+      name: "Stylis Jacket",
+      price: 120,
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=6",
+          altText: "Stylish Jacket",
+        },
+      ],
+    },
+    {
+      _id: "7",
+      name: "Stylis Jacket",
+      price: 120,
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=7",
+          altText: "Stylish Jacket",
+        },
+      ],
+    },
+    {
+      _id: "8",
+      name: "Stylis Jacket",
+      price: 120,
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=8",
+          altText: "Stylish Jacket",
+        },
+      ],
+    },
+  ];
 
   return (
     <div>
@@ -103,13 +105,22 @@ const placeholderProducts =[
       <GenderCollection></GenderCollection>
       <NewArrival></NewArrival>
       {/* Best Seller */}
-      <h2 className="text-3xl font-bold text-center mt-8 mb-4">Best Seller</h2>
-      <ProductDetails></ProductDetails>
+      <div>
+        <h2 className="text-3xl font-bold text-center mt-8 mb-8">
+          Best Seller
+        </h2>
+        <ProductDetails></ProductDetails>
+      </div>
 
-      <h2 className="text-3xl font-bold text-center mt-8 mb-4">
-        Top Wear For Women
-      </h2>
-      <ProductGrid products={placeholderProducts} ></ProductGrid>
+      <div className="container mx-auto">
+        <h2 className="text-3xl font-bold text-center mt-8 mb-4">
+          Top Wear For Women
+        </h2>
+        <ProductGrid products={placeholderProducts}></ProductGrid>
+      </div>
+
+      <FeaturedCollection></FeaturedCollection>
+      <FeaturesSection></FeaturesSection>
     </div>
   );
 };
